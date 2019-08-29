3157650f0fc92bbdb1f1ad19404bacd2d0c0356f
Daniela <aguileramalave@gmail.com>
2019-08-29 12:17:50 -0500

    Add app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { GlobalStyle } from './components/styles/GlobalStyle'
 import { Logo } from './components/Logo'
+import { Detail } from './pages/Detail'
+import { Home } from './pages/Home'
+import { NavBar } from './components/NavBar'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { Router } from '@reach/router'
+import Context from './Context'
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-  console.log(detailId)
   return (
     <div>
       <Logo />
       <GlobalStyle />
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={2} />
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+
+      </Router>
+      <Context.Consumer>
+        {
+          ({ isAuth }) =>
+            isAuth ? <Router>
+              <Favs path='/favs/' />
+              <User path='/user' />
+            </Router> : <Router>
+              <NotRegisteredUser path='/favs' />
+              <NotRegisteredUser path='/user' />
+            </Router>
+        }
+      </Context.Consumer>
+      <NavBar />
     </div>
   )
 }
