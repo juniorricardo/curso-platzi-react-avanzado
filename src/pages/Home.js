baaf211c15c1c2fe555172d49048603f8cbe1a11
Daniela <aguileramalave@gmail.com>
2019-09-10 18:31:39 +0200

    Add React.memo in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
-
-export const Home = ({ id }) => {
+import { Helmet } from 'react-helmet'
+const HomePage = ({ id }) => {
   return (
     <Fragment>
+      <Helmet>
+        <title>Petgram - Fotos de mascotas</title>
+        <meta name='description' content='Con petgram puedes encontrar fotos de animales' />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
     </Fragment>
   )
 }
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.id === props.id
+})
