6265f115d6801f24dbd13a04d4429304ad7cd185
Daniela <aguileramalave@gmail.com>
2019-09-10 18:25:29 +0200

    add react.lazy in app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import { GlobalStyle } from './components/styles/GlobalStyle'
+import React, { useContext, Suspense } from 'react'
+import { GlobalStyle } from './styles/GlobalStyle'
 import { Logo } from './components/Logo'
 import { Detail } from './pages/Detail'
 import { Home } from './pages/Home'
 import { NavBar } from './components/NavBar'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import { Favs } from './pages/Favs'
+import { NotFound } from './pages/NotFound'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 const UserLogged = ({ children }) => {
   return children({ isAuth: false })
 }
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <Logo />
       <GlobalStyle />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
-
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='login' />}
+        {!isAuth && <Redirect from='/user' to='login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs/' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth ? <Router>
-              <Favs path='/favs/' />
-              <User path='/user' />
-            </Router> : <Router>
-              <NotRegisteredUser path='/favs' />
-              <NotRegisteredUser path='/user' />
-            </Router>
-        }
-      </Context.Consumer>
       <NavBar />
-    </div>
+    </Suspense>
   )
 }
