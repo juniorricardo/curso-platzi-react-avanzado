f789b57b60e19d387f091eba4716de065ba46cf2
Daniela <aguileramalave@gmail.com>
2019-09-10 18:26:59 +0200

    add proptypes in FavButton

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from './styles.js'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import PropTypes from 'prop-types'
 
 export const FavButton = ({ liked, likes, onClick }) => {
   const Icon = liked ? MdFavorite : MdFavoriteBorder
@@ -9,4 +10,10 @@ export const FavButton = ({ liked, likes, onClick }) => {
       <Icon /> {likes} likes!
     </Button>
   )
+}
+
+FavButton.protoTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
