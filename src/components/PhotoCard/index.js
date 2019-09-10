bb3cf67b67d48edb1352797c521ed0e02a733fb1
Daniela <aguileramalave@gmail.com>
2019-09-10 18:28:55 +0200

    Add proptypes and styles in PhotoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react'
 import { ImgWrapper, Img, CardBottom, Article } from './styles.js'
 
-import { useNearScreen } from '../hooks/useNearScreen'
+import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleFavMutation'
 import { Link } from '@reach/router'
+import PropTypes from 'prop-types'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -36,4 +37,21 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       }
     </Article>
   )
+}
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function (props, propName, componentName) {
+    const propValue = props[propName]
+
+    if (propValue === undefined) {
+      return new Error(`${propName} value must be defined`)
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} value must be greater than 0`)
+    }
+  }
 }
